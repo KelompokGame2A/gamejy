1956105f17ba84ada5741c3ebe632b56facfe96e
Haris Zulfikar Islam <mirror_license@yahoo.co.id>
2017-05-20 21:30:06 +0700

    Create Member.java

---
+++ Member.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Member {
+    private String clan;
+    private String namaMbr;
+    private int th;
+    private int lv;
+    private int pr;
+    
+    public Member(){
+        this.clan = "";
+        this.namaMbr = "";
+        this.th = 0;
+        this.lv = 0;
+        this.pr = 0;
+    }
